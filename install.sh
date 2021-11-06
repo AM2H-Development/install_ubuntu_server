f5e36961862c3d9260b5528148d537384e66d0fd
martinhetschel <martin.hetschel@mach1kart.de>
2021-11-06 17:08:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ sudo timedatectl set-timezone Europe/Berlin
 sudo apt update && sudo apt upgrade
 sudo apt install net-tools mc
 
-sudo apt install cockpit cockpit-doc cockpit-podman cockpit-packagekit xdg-utils libparted-dev pcscd avahi-autoipd libteam-utils exfat-utils f2fs-tools nilfs-tools reiserfsprogs udftools udisks2-btrfs udisks2-lvm2 comgt wvdial wpagui libengine-pkcs11-openssl tuned
+sudo apt install cockpit cockpit-doc xdg-utils libparted-dev pcscd avahi-autoipd libteam-utils exfat-utils f2fs-tools nilfs-tools reiserfsprogs udftools udisks2-btrfs udisks2-lvm2 comgt wvdial wpagui libengine-pkcs11-openssl tuned
+sudo apt install cockpit-machines cockpit-storaged cockpit-packagekit cockpit-networkmanager cockpit-dashboard cockpit-bridge
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
 sudo apt remove docker docker-engine docker.io containerd runc
