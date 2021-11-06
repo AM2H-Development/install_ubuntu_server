0340fefd211b92e6a3ccfc78b0e9a93a9fd8db83
martinhetschel <martin.hetschel@mach1kart.de>
2021-11-06 15:59:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 sudo timedatectl set-timezone Europe/Berlin
 sudo apt update && sudo apt upgrade
 sudo apt install net-tools mc docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
